2f4ae1784883f13e391a15fd52b35fd5240cd12b
nnorman15 <nnorman15@gmail.com>
2020-07-10 15:59:20 -0700

    localStorge history now populates as a dropdown when clicking on the destination search box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,50 @@
+var searchFormEl = document.querySelector("#search-form");
+var searchInputEl = document.querySelector("#searchTxtInput");
+var historyContainerEl = document.querySelector("#searchDatalist");
+
+//create history dropdown elements in hike search field
+var createHistoryDropdown = function(){
+    //console.log("createHistoryDropdown");
+    historyContainerEl.innerHTML = "";
+    var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryArr"));
+    if (searchHistoryArr != null){
+        searchHistoryArr = searchHistoryArr.sort();
+        //console.log(searchHistoryArr);
+        for (var i=0;i<searchHistoryArr.length;i++){
+            //console.log(searchHistoryArr[i]);
+            var historyListItem = document.createElement("option");
+            historyListItem.value = searchHistoryArr[i];
+            historyListItem.text = searchHistoryArr[i];
+            historyContainerEl.appendChild(historyListItem);
+        }
+
+    }
+
+}
+
+//store search in localStorage
+var storeSearchHistory = function(searchValue){
+    //console.log(searchValue);
+    
+    //strip search value of leading/trailing spaces and lowercase
+    var cleanedSearchValue = searchValue.toLowerCase().trim();
+    var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryArr"));
+
+    //console.log(searchHistoryArr);
+
+    //if localstorage var doesn't exist
+    if (searchHistoryArr === null){
+        searchHistoryArr = [];
+    }
+
+    //avoids duplicates - if searched value does not already exist in localStorage array, add it
+    if (searchHistoryArr.indexOf(cleanedSearchValue) < 0){
+        searchHistoryArr.push(cleanedSearchValue);
+        localStorage.setItem("searchHistoryArr", JSON.stringify(searchHistoryArr));
+    }
+
+}
+
 
 
 function getCityCoord(city, state) {
@@ -39,3 +86,24 @@ function getHikingInfo(lat, lon) {
 function displayTrails(trails) {
     
 }
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+
+    //console.log(event);
+
+    // get value from input element
+    var searchValue = searchInputEl.value.trim();
+    
+    //console.log(cityname);
+
+    if (searchValue) {
+        storeSearchHistory(searchValue);
+        searchInputEl.value = "";
+    } else {
+        //TODO : VALIDATION MODAL TO GO HERE TO STATE THAT USER NEEDS TO ENTER A VALID DESTINATION (NOT BLANK)
+    }
+}
+
+searchFormEl.addEventListener("submit", formSubmitHandler);
+searchInputEl.addEventListener("focus", createHistoryDropdown);
