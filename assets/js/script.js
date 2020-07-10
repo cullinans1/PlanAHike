0eeab0a9071e1cd384cad3e7eedcb1057bf55056
cassie.cattabiani@gmail.com <cassie.cattabiani@gmail.com>
2020-07-10 14:28:09 -0700

    added modal button to cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,148 @@
-//NEED TO ADJUST CODE BASED ON COMPLETION OF HTML FILE AND HIKING TRAIL RESULTS
-//DYNAMICALLY CREATE HTML/CSS FOR MODULE THROUGH JS FILE
+var cardDisplayEl = document.getElementById("cards");
+var searchBtnEl = document.getElementById("search-button");
+var citySearchEl = document.getElementById("search-term");
 
-// Get the modal
-var modal = document.getElementById('myModal'); 
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn"); 
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+var getCityHandler = function(event) {
+    event.preventDefault();
+    var cityName = citySearchEl.value.trim();
+    if (cityName) {
+        getCityCoord(cityName);
+        citySearchEl.value= "";
+    } else {
+        return;
     }
 }
+function getCityCoord(city, state) {
+    
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&units=imperial&appid=7b788606d2ca3b8dec8a6e5ab63f1a3c")
+    .then(function(response){
+        if (response.ok) {
+            response.json().then(function(data) {
+            getHikingInfo(data.coord.lat, data.coord.lon);
+            forecastWeather(data.coord.lat, data.coord.lon);
+            });
+        } else {
+            
+        }
+    });
+};
+function forecastWeather(lat, lon) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=7b788606d2ca3b8dec8a6e5ab63f1a3c")
+    .then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+}
+function getHikingInfo(lat, lon) {
+    fetch("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=50&maxResults=30&key=200829481-354572aba0151d42b45ec3c006e7cbef")
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data.trails)
+                displayTrails(data, data.trails)
+            });
+        } else {
+            
+        }
+    });
+}
+function displayTrails(data, trails) {
+    //clear out previous data
+    cardDisplayEl.textContent = "";
+    for(var i = 0; i < trails.length; i++ ) {
+        //for image
+        if(trails[i].imgMedium !== "" ) {
+            var calloutContainer = document.createElement("div");
+            calloutContainer.classList = "column"
+            var callout = document.createElement("div");
+            callout.classList = "callout";
+            var calloutImg = document.createElement("img");
+            calloutImg.setAttribute("src", trails[i].imgMedium);
+            callout.appendChild(calloutImg);
+            calloutContainer.appendChild(callout);
+        } else {
+            var calloutContainer = document.createElement("div");
+            calloutContainer.classList = "column"
+            var callout = document.createElement("div");
+            callout.classList = "callout";
+            var calloutImg = document.createElement("img");
+            calloutImg.setAttribute("src", "assets/images/mountain.png");
+            //Icons made by <a href="https://www.flaticon.com/authors/pongsakornred" title="pongsakornRed">pongsakornRed</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            callout.appendChild(calloutImg);
+            calloutContainer.appendChild(callout);
+        };
+        
+        //for title of hike
+        var hikeTitle = document.createElement("p");
+        hikeTitle.classList = "lead";
+        hikeTitle.textContent = trails[i].name;
+        callout.appendChild(hikeTitle);
+
+        var hikeLocation = document.createElement('p');
+        hikeLocation.classList = "subheader";
+        hikeLocation.textContent = trails[i].location;
+        callout.appendChild(hikeLocation);
+
+         //button that opens modal
+        var modalButton = document.createElement("button");
+        modalButton.textContent = "See trial details";
+        modalButton.classList.add("modalBtn");
+        modalButton.id = "myModal";
+        callout.appendChild(modalButton);
+
+        //append all to page
+        cardDisplayEl.appendChild(calloutContainer);
+
+        
+       
+       
+
+
+        
+    }
+
+}
+
+//event listeners
+searchBtnEl.addEventListener("click", getCityHandler);
+
+
+
+
+
+
+
+
+
+
+
+// //NEED TO ADJUST CODE BASED ON COMPLETION OF HTML FILE AND HIKING TRAIL RESULTS
+// //DYNAMICALLY CREATE HTML/CSS FOR MODULE THROUGH JS FILE
+
+// // Get the modal
+// var modal = document.getElementById('myModal'); 
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn"); 
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
