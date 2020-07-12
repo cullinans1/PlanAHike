f7c74fb09f59253fe7599eed1393c6491e384a26
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-07-12 15:28:34 -0700

    updated css/html

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,6 +1,4 @@
 var cardDisplayEl = document.getElementById("cards");
-var searchBtnEl = document.getElementById("search-button");
-//var citySearchEl = document.getElementById("search-term");
 var span = document.getElementsByClassName("close")[0];
 var modal = document.getElementById('myModal');
 var searchFormEl = document.querySelector("#search-form");
@@ -10,6 +8,9 @@ var loadMoreEl = document.getElementById("load-more");
 var removeHiddenEl = document.querySelector(".loadBtn");
 var instructionsEl = document.querySelector(".entry");
 var noResultsEl = document.querySelector(".no-results")
+var forecastContainerEl = document.querySelector("#forecast");
+var todayD = moment().format("MM/DD/YYYY");
+
 
 //create history dropdown elements in hike search field
 var createHistoryDropdown = function(){
@@ -27,7 +28,6 @@ var createHistoryDropdown = function(){
             historyContainerEl.appendChild(historyListItem);
         }
     }
-
 }
 
 //store search in localStorage
@@ -50,100 +50,91 @@ var storeSearchHistory = function(searchValue){
         searchHistoryArr.push(cleanedSearchValue);
         localStorage.setItem("searchHistoryArr", JSON.stringify(searchHistoryArr));
     }
-
 }
 
-// var getCityHandler = function(event) {
-//     event.preventDefault();
-//     var cityName = searchInputEl.value.trim();
-//     if (cityName) {
-//         getCityCoord(cityName);
-//         searchInputEl.value= "";
-//     } else {
-//         return;
-//     }
-// }
-
 function getCityCoord(city, state) {
-    
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&units=imperial&appid=7b788606d2ca3b8dec8a6e5ab63f1a3c")
     .then(function(response){
         if (response.ok) {
             response.json().then(function(data) {
             getSelectedValue(data.coord.lat, data.coord.lon); //removed getHikingInfo call and replaced with getSelectedValue (getHikingInfo is called in getSelectedValue)
-            forecastWeather(data.coord.lat, data.coord.lon);
+            forecastWeather(data.coord.lat, data.coord.lon, city, data);
             });
         } else {
-          noResultsEl.removeAttribute("id", "hidden");
-          instructionsEl.setAttribute("id", "hidden");  
-          return; 
+            noResultsEl.removeAttribute("id", "hidden");
+            instructionsEl.setAttribute("id", "hidden");
+            return;
         }
     });
 }
 
-function forecastWeather(lat, lon) {
+function forecastWeather(lat, lon, city, nowWeather) {
     fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=7b788606d2ca3b8dec8a6e5ab63f1a3c")
     .then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        })
-    })
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayForecast(data,nowWeather);
+            });
+        }
+    });
 }
 
 //function that gets lat and long ONLY
 function getHikingInfo(lat, lon) {
-  fetch("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=50&maxResults=30&key=200829481-354572aba0151d42b45ec3c006e7cbef")
-  .then(function(response) {
-      if (response.ok) {
-          response.json().then(function(data) {
-              console.log(data.trails)
-              displayTrails(data, data.trails)
-              //show load more button
-              removeHiddenEl.removeAttribute("id", "hidden");
-          });
-      } else {
-          noResultsEl.removeAttribute("id", "hidden");
-          return;
-      }
-  });
-}
-
-//function that gets the sort
-function getHikingSort(result, lat, lon) {
+  var selectedItem = getSelectedValue();
   // console.log(selectedItem);
-    fetch("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=50&maxResults=30&key=200829481-354572aba0151d42b45ec3c006e7cbef" + "&sort=" + result)
+    fetch("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=50&maxResults=30&key=200829481-354572aba0151d42b45ec3c006e7cbef" /*+ "&sort=" + selectedItem*/)
+
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data.trails)
+                //console.log(data.trails)
                 displayTrails(data, data.trails)
                 //show load more button
-              removeHiddenEl.removeAttribute("id", "hidden");
+                removeHiddenEl.removeAttribute("id", "hidden");
             });
         } else {
-          noResultsEl.removeAttribute("id", "hidden");
-          return;
+            noResultsEl.removeAttribute("id", "hidden");
+            return;
         }
     });
 }
 
-// Function that grabs getHikingInfo and getHikingSort
+//function that gets the sort
+function getHikingSort(result, lat, lon) {
+    // console.log(selectedItem);
+      fetch("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=50&maxResults=30&key=200829481-354572aba0151d42b45ec3c006e7cbef" + "&sort=" + result)
+      .then(function(response) {
+          if (response.ok) {
+              response.json().then(function(data) {
+                  console.log(data.trails)
+                  displayTrails(data, data.trails)
+                  //show load more button
+                removeHiddenEl.removeAttribute("id", "hidden");
+              });
+          } else {
+            noResultsEl.removeAttribute("id", "hidden");
+            return;
+          }
+      });
+  }
+
+  // Function that grabs getHikingInfo and getHikingSort
 function getSelectedValue (lat, lon) {
-  var list = document.getElementById("myList");
-  var result = list.options[list.selectedIndex].value;
-  console.log(result)
-  if(result !== "") {
-    console.log("nul not selected")
-    getHikingSort(result, lat, lon);
+    var list = document.getElementById("myList");
+    var result = list.options[list.selectedIndex].value;
+    console.log(result)
+    if(result !== "") {
+      console.log("nul not selected")
+      getHikingSort(result, lat, lon);
+    }
+    else{
+      getHikingInfo(lat, lon) //if sort is not specified, only call getHikingInfo
+    }
   }
-  else{
-    getHikingInfo(lat, lon) //if sort is not specified, only call getHikingInfo
-  }
-}
-
-
 
 function displayTrails(data, trails) {
+
     //remove instructions
     instructionsEl.setAttribute("id", "hidden");
     //clear out previous data
@@ -217,6 +208,7 @@ function displayTrails(data, trails) {
 
         //append all to page
         cardDisplayEl.appendChild(calloutContainer);
+
     }
     //slicing data to display on page
     var firstSliceValue = 0
@@ -225,9 +217,190 @@ function displayTrails(data, trails) {
     //var firstSlice = data.trails.slice(firstSliceValue, sliceValue);
     function addSliceValue () {
         event.preventDefault();
-        sliceValue += 6
-        firstSliceValue += 6
-        var slicedValue = firstSlice = data.trails.slice(firstSliceValue, sliceValue)
+        sliceValue += 6;
+        firstSliceValue += 6;
+        var slicedValue = data.trails.slice(firstSliceValue, sliceValue);
+        console.log(data);
+        slicedResults(slicedValue);
+        console.log(slicedValue);
+    }
+    loadMoreEl.addEventListener("click", addSliceValue);
+    function slicedResults (slicedValue) {
+    for(var i = 0; i < slicedValue.length; i++ ) {
+        if(slicedValue[i].imgMedium !== "" ) {
+            var calloutContainer = document.createElement("div");
+            calloutContainer.classList = "column"
+            var callout = document.createElement("div");
+            callout.classList = "callout";
+            var calloutImg = document.createElement("img");
+            calloutImg.setAttribute("src", slicedValue[i].imgMedium);
+            callout.appendChild(calloutImg);
+            calloutContainer.appendChild(callout);
+        } else {
+            var calloutContainer = document.createElement("div");
+            calloutContainer.classList = "column"
+            var callout = document.createElement("div");
+            callout.classList = "callout";
+            var calloutImg = document.createElement("img");
+            calloutImg.setAttribute("src", "assets/images/mountain.png");
+            //Icons made by <a href="https://www.flaticon.com/authors/pongsakornred" title="pongsakornRed">pongsakornRed</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            callout.appendChild(calloutImg);
+            calloutContainer.appendChild(callout);
+        };
+        
+        //for title of hike
+        var hikeTitle = document.createElement("p");
+        hikeTitle.classList = "lead";
+        hikeTitle.textContent = slicedValue[i].name;
+        callout.appendChild(hikeTitle);
+
+        var hikeLocation = document.createElement('p');
+        hikeLocation.classList = "subheader";
+        hikeLocation.style = "color: black;"
+        hikeLocation.textContent = slicedValue[i].location;
+        callout.appendChild(hikeLocation);
+
+        var hikeSummary = document.createElement('p');
+        hikeSummary.classList = "subheader";
+        hikeSummary.textContent = slicedValue[i].summary;
+        callout.appendChild(hikeSummary);
+
+        //button that opens modal
+        var modalButton = document.createElement("button");
+        modalButton.textContent = "See trial details";
+        modalButton.classList.add("modalBtn");
+        modalButton.setAttribute("data-id", i);
+        modalButton.id = "myBtn";
+        callout.appendChild(modalButton);
+
+
+        // when the user clicks on the button, open modal
+        modalButton.onclick = function(e) {
+            const thisTrail = slicedValue[parseInt(e.target.dataset.id)]
+            showModal(thisTrail);
+        }
+
+        //when the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        //When the user clicks anywhere outside of modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+        //append all to page
+        cardDisplayEl.appendChild(calloutContainer);
+    }
+    }
+}
+
+var displayForecast = function(data,nowWeather) {
+    //console.log("displayForecast");
+    //console.log(data);
+    //console.log(nowWeather);
+
+    //weather container content
+    forecastContainerEl.textContent = "";
+
+    // check if api returned any forecast data
+    if (data.length === 0) {
+        forecastContainerEl.textContent = "No forecast data found for " + data.city.name + ".";
+        return;
+    }
+
+    //create h2 3-day forecast header
+    var forecastTitleRow = document.createElement("div");
+    forecastTitleRow.classList = "row forecastTitle";
+    forecastTitleRow.innerHTML = "<div class='small-12 large-12 columns'><h2>3 Day Forecast for " + data.city.name + "</h2></div>";
+
+    //create forecast container for all the items
+    var forecastDaysContainer = document.createElement("div");
+    forecastDaysContainer.classList = "row forecastAllContainer";
+
+    // append containers to the dom
+    forecastContainerEl.append(forecastTitleRow,forecastDaysContainer);
+
+    //using this variable to populate the forecastData array within the loop over the forecast data returned by the api
+    var forecastDataIndex = 1;
+
+    //create array that will contain day objects for the forecast
+    let forecastData = [];
+
+    //current day's weather comes from diff source than forecast.  populate first item in array with current weather
+    forecastData[0] = {
+        "date" : todayD, 
+        "icon" : "https://openweathermap.org/img/wn/" + nowWeather.weather[0].icon + ".png", 
+        "temp" : nowWeather.main.temp, 
+        "humidity" : nowWeather.main.humidity
+    };
+
+    nextD = moment().add(1,'days').format("MM/DD/YYYY");
+
+    //loop over api forecast data and build object array for the next 2 days
+    for (var apiFCData=0;apiFCData<data.list.length;apiFCData++){
+        //console.log("apiFCData loop " + apiFCData + " : " + moment(data.list[apiFCData].dt_txt).format("MM/DD/YYYY"));
+        if (nextD == moment(data.list[apiFCData].dt_txt).format("MM/DD/YYYY")){
+            //console.log("nextD : " + nextD);
+
+            forecastData[forecastDataIndex] = {
+                "date" : nextD, 
+                "icon" : "https://openweathermap.org/img/wn/" + data.list[apiFCData].weather[0].icon + ".png", 
+                "temp" : data.list[apiFCData].main.temp, 
+                "humidity" : data.list[apiFCData].main.humidity
+            };
+            
+            //increment forecastDataIndex by 1 to populate the array objects
+            forecastDataIndex = forecastDataIndex+1;
+
+            //increment nextD by 1 day
+            nextD = moment(new Date(nextD)).add(1,'days').format("MM/DD/YYYY");
+        }
+    }
+    //console.log(forecastData);
+
+    //loop over forecastData array to build html objects
+    for (var i=0;i<3;i++){
+        //console.log(forecastData[i].date);
+        
+        var forecastDayParentContainer = document.createElement("div");
+            forecastDayParentContainer.classList = "small-12 medium-12 large-4 columns forecastDayContainer";
+        var forecastDayChildContainer = document.createElement("div");
+            forecastDayChildContainer.classList = "forecastDayItem";
+        var forecastDayDateEl = document.createElement("div");
+            forecastDayDateEl.classList = "forecastDate";
+            forecastDayDateEl.innerText = forecastData[i].date;
+        var forecastDayIconEl = document.createElement("div");
+            var weatherIcon = document.createElement("img");
+            weatherIcon.setAttribute("src", forecastData[i].icon);
+            forecastDayIconEl.append(weatherIcon);
+        var forecastDayTempEl = document.createElement("div");
+            forecastDayTempEl.innerText = "Temp: " + forecastData[i].temp + String.fromCharCode(176) + "F";
+        var forecastDayHumidyEl = document.createElement("div");
+            forecastDayHumidyEl.innerText = "Humidity: " + forecastData[i].humidity + "%";
+
+        //add individual elements to the parent forecastDayContainerEl
+        forecastDayChildContainer.append(forecastDayDateEl,forecastDayIconEl,forecastDayTempEl,forecastDayHumidyEl);
+        forecastDayParentContainer.append(forecastDayChildContainer);
+        //add container to the dom
+        forecastContainerEl.append(forecastDayParentContainer);
+        
+
+    }
+    //slicing data to display on page
+    var firstSliceValue = 0
+    var sliceValue = 6
+    
+    //var firstSlice = data.trails.slice(firstSliceValue, sliceValue);
+    function addSliceValue () {
+        event.preventDefault();
+        sliceValue += 6;
+        firstSliceValue += 6;
+        var slicedValue = data.trails.slice(firstSliceValue, sliceValue);
+        console.log(data);
         slicedResults(slicedValue);
         console.log(slicedValue);
     }
@@ -321,7 +494,6 @@ function showModal(data){
     descent.textContent = "Descent: " + data.descent + " ft"; 
 }
 
-
 var formSubmitHandler = function(event){
     event.preventDefault();
 
@@ -336,6 +508,7 @@ var formSubmitHandler = function(event){
         storeSearchHistory(searchValue);
         getCityCoord(searchValue);
         searchInputEl.value = "";
+        searchInputEl.blur();
     } else {
         //TODO : VALIDATION MODAL TO GO HERE TO STATE THAT USER NEEDS TO ENTER A VALID DESTINATION (NOT BLANK)
     }
@@ -343,6 +516,5 @@ var formSubmitHandler = function(event){
 
 
 //event listeners
-// searchBtnEl.addEventListener("click", getCityHandler);
 searchFormEl.addEventListener("submit", formSubmitHandler);
 searchInputEl.addEventListener("focus", createHistoryDropdown);
