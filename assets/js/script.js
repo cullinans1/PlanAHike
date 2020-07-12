81f67e263e3f826f23642f3edeab7e5082d37a7f
cassie.cattabiani@gmail.com <cassie.cattabiani@gmail.com>
2020-07-11 18:50:30 -0700

    added API call and updated functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ var removeHiddenEl = document.querySelector(".loadBtn");
 var instructionsEl = document.querySelector(".entry");
 var noResultsEl = document.querySelector(".no-results")
 
-
 //create history dropdown elements in hike search field
 var createHistoryDropdown = function(){
     //console.log("createHistoryDropdown");
@@ -71,13 +70,13 @@ function getCityCoord(city, state) {
     .then(function(response){
         if (response.ok) {
             response.json().then(function(data) {
-            getHikingInfo(data.coord.lat, data.coord.lon);
+            getSelectedValue(data.coord.lat, data.coord.lon); //removed getHikingInfo call and replaced with getSelectedValue (getHikingInfo is called in getSelectedValue)
             forecastWeather(data.coord.lat, data.coord.lon);
             });
         } else {
-            noResultsEl.removeAttribute("id", "hidden");
-            instructionsEl.setAttribute("id", "hidden");
-            return;
+          noResultsEl.removeAttribute("id", "hidden");
+          instructionsEl.setAttribute("id", "hidden");  
+          return; 
         }
     });
 }
@@ -91,28 +90,60 @@ function forecastWeather(lat, lon) {
     })
 }
 
+//function that gets lat and long ONLY
 function getHikingInfo(lat, lon) {
-  var selectedItem = getSelectedValue();
-  // console.log(selectedItem);
-    fetch("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=50&maxResults=30&key=200829481-354572aba0151d42b45ec3c006e7cbef" /*+ "&sort=" + selectedItem*/)
+  fetch("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=50&maxResults=30&key=200829481-354572aba0151d42b45ec3c006e7cbef")
+  .then(function(response) {
+      if (response.ok) {
+          response.json().then(function(data) {
+              console.log(data.trails)
+              displayTrails(data, data.trails)
+              //show load more button
+              removeHiddenEl.removeAttribute("id", "hidden");
+          });
+      } else {
+          noResultsEl.removeAttribute("id", "hidden");
+          return;
+      }
+  });
+}
 
+//function that gets the sort
+function getHikingSort(result, lat, lon) {
+  // console.log(selectedItem);
+    fetch("https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=50&maxResults=30&key=200829481-354572aba0151d42b45ec3c006e7cbef" + "&sort=" + result)
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data.trails)
                 displayTrails(data, data.trails)
                 //show load more button
-                removeHiddenEl.removeAttribute("id", "hidden");
+              removeHiddenEl.removeAttribute("id", "hidden");
             });
         } else {
-            noResultsEl.removeAttribute("id", "hidden");
-            return;
+          noResultsEl.removeAttribute("id", "hidden");
+          return;
         }
     });
 }
 
-function displayTrails(data, trails) {
+// Function that grabs getHikingInfo and getHikingSort
+function getSelectedValue (lat, lon) {
+  var list = document.getElementById("myList");
+  var result = list.options[list.selectedIndex].value;
+  console.log(result)
+  if(result !== "") {
+    console.log("nul not selected")
+    getHikingSort(result, lat, lon);
+  }
+  else{
+    getHikingInfo(lat, lon) //add function that calls just lat and lon here
+  }
+}
 
+
+
+function displayTrails(data, trails) {
     //remove instructions
     instructionsEl.setAttribute("id", "hidden");
     //clear out previous data
@@ -194,10 +225,9 @@ function displayTrails(data, trails) {
     //var firstSlice = data.trails.slice(firstSliceValue, sliceValue);
     function addSliceValue () {
         event.preventDefault();
-        sliceValue += 6;
-        firstSliceValue += 6;
-        var slicedValue = data.trails.slice(firstSliceValue, sliceValue);
-        console.log(data);
+        sliceValue += 6
+        firstSliceValue += 6
+        var slicedValue = firstSlice = data.trails.slice(firstSliceValue, sliceValue)
         slicedResults(slicedValue);
         console.log(slicedValue);
     }
@@ -291,16 +321,6 @@ function showModal(data){
     descent.textContent = "Descent: " + data.descent + " ft"; 
 }
 
-// Testing drop down values - start
-
-// Grabbing drop down values
-function getSelectedValue () {
-  var list = document.getElementById("myList");
-  var result = list.options[list.selectedIndex].value;
-  console.log(result)
-}
-
-//Testing drop down values - end
 
 var formSubmitHandler = function(event){
     event.preventDefault();
