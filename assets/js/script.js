3a4d1974086506ffd18643a71c21f6b0147f0851
Sammy <sammy.cullinan@gmail.com>
2020-07-13 14:04:51 -0700

    fixed error with modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -244,23 +244,23 @@ function displayTrails(data, trails) {
             showModal(thisTrail);
         }
 
-        //when the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        //When the user clicks anywhere outside of modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
 
         searchResultsTitle.innerHTML = "<h2>Hikes<h2>";
 
         //append all to page
         cardDisplayEl.appendChild(calloutContainer);
 
+    }
+}
+//when the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+//When the user clicks anywhere outside of modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
 //var firstSlice = data.trails.slice(firstSliceValue, sliceValue);
